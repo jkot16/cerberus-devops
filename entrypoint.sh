dee0a515c7a6c4ca2da4bdee44361f44c4f773ab
Jakub Kot <kotjakub16@gmail.com>
2025-04-26 13:37:17 +0200

    Chore: remove legacy crontab from EC2 host

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Create /status endpoint log file
 touch /app/cerberus.log
 
+# Export webhook URL for cron to access
+printenv CERBERUS_WEBHOOK_URL >> /etc/environment
+
 # Start the Flask server in the background
 python app.py &
 
